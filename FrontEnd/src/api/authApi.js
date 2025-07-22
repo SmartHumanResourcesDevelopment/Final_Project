49b38169adae3483a99ae1d5f4594079cfa98db8
qldfhr123 <alfowko3258@naver.com>
2025-07-22 14:01:36 +0900

    axios

--- FrontEnd/src/api/authApi.js
+++ FrontEnd/src/api/authApi.js
@@ -1,6 +1,7 @@
 // src/api/authApi.js
 import axios from "axios";
 
+// npm install axios 필요
 export const signUp = async (data) => {
   const res = await axios.post("http://localhost:8080/api/join", data);
   return res.data;           // { success, message }
