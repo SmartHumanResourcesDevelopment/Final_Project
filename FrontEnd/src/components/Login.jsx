3dd9ef801f8001228466905b3d80e28ad7379632
ckaudgns89 <ckaudgns89@naver.com>
2025-07-24 15:08:04 +0900

    로그인 회원가입 기능 보완

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -19,7 +19,6 @@ const Login = () => {
       const response = await login({ id, password });
       if (response.success) {
         console.log("로그인 성공:", response.user);
-        alert("로그인 성공!");
         navigate("/main"); // 홈 또는 마이페이지 이동
       } else {
         alert(`로그인 실패: ${response.message}`);
